2fd9d9d6fb5b53ffc5af12c4c685ad88ada76c52
Unknown <mzq323@alumni.ku.dk>
2019-10-22 16:22:12 +0800

    Fixed fragment issues
    Instead of making a spending in home_fragment, we now have a makeSpending fragment that makes a spending. This also means that we don't have to run these methods through Main, but instead through another class which extends Fragments. Sick!

--- wheresmymoney2/app/src/main/java/com/example/wheresmymoney/makeSpending.java
+++ wheresmymoney2/app/src/main/java/com/example/wheresmymoney/makeSpending.java
@@ -1,19 +1,31 @@
 package com.example.wheresmymoney;
 
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.time.Instant;
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class makeSpending extends Fragment {
+    TextView output;
+    Button addSpending;
+    Database DB = new Database();
+    EditText editText;
 
 
     public makeSpending() {
@@ -26,6 +38,16 @@ public class makeSpending extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_make_spending, container, false);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void makeSpending(View v){
+        output.setText(editText.getText().toString());
+        float spendingAmount = Float.parseFloat(editText.getText().toString());
+
+        Date newdate = Date.from(Instant.now());
+        //Category category =
+        DB.MakeSpending(spendingAmount, newdate, DB.Categories.get(0));
+        //output.setText(DB.Spendings.get(0).amount + DB.Spendings.get(0).date.toString() + DB.Spendings.get(0).category.name +"");
     }
 
 }
